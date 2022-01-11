b4652c5f66c5a6101010306f443fa85dae9e9771
Kevin Orta <orta.kevin@gmail.com>
2022-01-11 09:35:19 -0600

    finished occupation functionality

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 const API = "https://frontend-take-home.fetchrewards.com/form";
 
+
 $.get(API).done(function (data){
     console.log(data);
+    $("#occupationID").text("");
+
+    var occups = "";
+
+
+    for (var i = 0; i <= 12; i += 1){
+        console.log(data.occupations[i]);
+        occups += "<option>" + data.occupations[i] + "</option>"
+        // $("occupationID").html(
+        //     "<div>" +
+        //     "<p>" + data.occupations[i] + "</p>" +
+        //     "</div>"
+        // )
+    }
+    $("#inputGroupSelect01").html(occups);
 })
